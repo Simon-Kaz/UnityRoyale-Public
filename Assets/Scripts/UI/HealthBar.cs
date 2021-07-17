ee36b65d67c27b90a63baf0ce4888fb6c45a9e47
Szymon Kazmierczak <skazmierczak@zendesk.com>
2021-07-17 15:17:26 +0100

    move logic from monobehaviour to simple c# class

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityRoyale;
 
 namespace UnityRoyale
 {
@@ -9,49 +8,65 @@ namespace UnityRoyale
     {
         public RectTransform bar;
         public GameObject wholeWidget;
+        private HealthBarController healthBarController;
+        private bool _isHidden = true;
 
-        private bool isHidden = true;
-        private float originalHP;
-        private float currentHP;
-        private Transform transformToFollow;
-
-		private Color red = new Color32(252, 35, 13, 255);
-		private Color blue = new Color32(31, 132, 255, 255);
-
-        public void Initialise(ThinkingPlaceable p)
+        public void Initialise(ThinkingPlaceable placeable)
         {
-            originalHP = currentHP = p.hitPoints;
-            transformToFollow = p.transform;
+            healthBarController = new HealthBarController(placeable);
 
-            bar.GetComponent<Image>().color = (p.faction == Placeable.Faction.Player) ? red : blue;
-            
-            wholeWidget.transform.localPosition = new Vector3(0f,
-															(p.pType == Placeable.PlaceableType.Unit) ? 3f : 6f,
-															(p.pType == Placeable.PlaceableType.Unit) ? 0f : -2f); //set the vertical position based on the type of Placeable
+            bar.GetComponent<Image>().color = healthBarController.BarColor;
+            wholeWidget.transform.localPosition = healthBarController.LocalPosition;
             wholeWidget.SetActive(false);
         }
 
         public void SetHealth(float newHP)
         {
-            if(isHidden)
+            if (_isHidden)
             {
                 wholeWidget.SetActive(true);
-                isHidden = false;
+                _isHidden = false;
             }
 
-            float ratio = 0f;
-            if(newHP > 0f)
-            {
-                ratio = newHP/originalHP;
-            }
+            var ratio = healthBarController.CalculateRatio(newHP);
 
             bar.localScale = new Vector3(ratio, 1f, 1f);
         }
 
         public void Move()
         {
-            if(transformToFollow != null)
-                transform.position = transformToFollow.position;
+            if (healthBarController.TransformToFollow != null) //TODO: do i still need this
+                transform.position = healthBarController.TransformToFollow.position;
         }
+    }
+}
+
+public class HealthBarController
+{
+    public Transform TransformToFollow { get; }
+    public Color BarColor { get; }
+    public Vector3 LocalPosition { get; }
+
+    private float _currentHp;
+    private readonly float _originalHp;
+
+    private readonly Color _red = new Color32(252, 35, 13, 255);
+    private readonly Color _blue = new Color32(31, 132, 255, 255);
+
+    public HealthBarController(ThinkingPlaceable placeable)
+    {
+        _originalHp = _currentHp = placeable.hitPoints;
+        TransformToFollow = placeable.transform;
+        BarColor = placeable.faction == Placeable.Faction.Player ? _red : _blue;
+        LocalPosition = new Vector3(0f,
+            (placeable.pType == Placeable.PlaceableType.Unit) ? 3f : 6f,
+            (placeable.pType == Placeable.PlaceableType.Unit)
+                ? 0f
+                : -2f); //set the vertical position based on the type of Placeable
+    }
+
+    public float CalculateRatio(float newHp)
+    {
+        return newHp > 0f ? newHp / _originalHp : 0f;
     }
 }
